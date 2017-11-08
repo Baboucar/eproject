e53f4943e20a1848e108d1c99f4305f7559201fb
Baboucarr <baboucarrd43@gmail.com>
2017-11-08 11:26:34 +0000

    first commit

---
+++ assets/scripts/js/main.js
@@ -0,0 +1,8 @@
+
+   function show(id){
+     var element =document.getElementById(id);
+     if(element.style.display=='none')
+       element.style.display='block';
+     else
+       element.style.display='none';
+}
